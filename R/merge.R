2febc70d020f34782aff5dba2c6b2731cc576783
Joshua Krusell <js.shirin@gmail.com>
2019-11-05 10:01:31 +0100

    Remove empty levels from month factor and vary all weekdays

--- R/merge.R
+++ R/merge.R
@@ -8,11 +8,12 @@ db <- DBI::dbConnect(RSQLite::SQLite(), "data/db")
 df <- tbl(db, "aggregated") %>%
     collect(n = Inf) %>%
     mutate(datestr = as.Date(datestr),
-           month = month(datestr, T),
-           weekday = weekdays(datestr),
-           strata = ifelse(weekday %in% c("Friday", "Saturday", "Sunday"),
-                           weekday,
-                           "Mon-Thurs") %>% as.factor)
+           year = year(datestr),
+           month = month(datestr, T) %>% droplevels,
+           weekday = weekdays(datestr) %>%
+               factor(ordered = T,
+                      levels = c("Monday", "Tuesday", "Wednesday", "Thursday",
+                                 "Friday", "Saturday", "Sunday")))
 
 weather <- readRDS("data/weather.rds") %>%
     select(-referenceTime)
